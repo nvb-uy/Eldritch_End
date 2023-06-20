8e81f6a3ddf15e7d7e0f08b995802efb2a093b38
nvb-uy <almhak@proton.me>
2023-06-20 02:57:43 -0300

    feat(faster fog)

--- src/main/java/elocindev/eldritch_end/mixin/BackgroundRendererMixin.java
+++ src/main/java/elocindev/eldritch_end/mixin/BackgroundRendererMixin.java
@@ -2,11 +2,15 @@ package elocindev.eldritch_end.mixin;
 import com.mojang.blaze3d.systems.RenderSystem;
 
 import elocindev.eldritch_end.registry.BiomeRegistry;
+import elocindev.eldritch_end.registry.BlockRegistry;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.CameraSubmersionType;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.biome.Biome;
 
@@ -25,15 +29,20 @@ public abstract class BackgroundRendererMixin {
 	@Inject(method = "applyFog", at = @At("TAIL"))
 	private static void eldritchFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo ci) {
 		MinecraftClient client = MinecraftClient.getInstance();
-		var world = client.world;
+		ClientWorld world = client.world;
+
 		Optional<RegistryKey<Biome>> biomeKey = world.getBiome(camera.getBlockPos()).getKey();
-		if (world == null || world.getBiome(camera.getBlockPos()).getKey().isEmpty() || !camera.getSubmersionType().equals(CameraSubmersionType.NONE))
+		if (world == null || !camera.getSubmersionType().equals(CameraSubmersionType.NONE))
 			return;
 		
-
-		if (biomeKey.get() == BiomeRegistry.PRIMORDIAL_ABYSS) {
-			RenderSystem.setShaderFogStart(MathHelper.lerp(1.0f, vanillaFogStart(viewDistance), 0f + 0.0f));
-			RenderSystem.setShaderFogEnd(MathHelper.lerp(1.0f, viewDistance, viewDistance / 3 + 0.0f));
+		Vec3d pos = client.player.getPos();
+		for (int i = 0; i < 8; i++) {
+			var bp = new BlockPos(pos.add(0, -i, 0));
+			if (world.getBlockState(bp).getBlock() == BlockRegistry.ABYSMAL_FRONDS || world.getBlockState(bp).getBlock() == BlockRegistry.SUSPICIOUS_FRONDS) {
+				RenderSystem.setShaderFogStart(MathHelper.lerp(1.0f, vanillaFogStart(viewDistance), 0f + 0.0f));
+				RenderSystem.setShaderFogEnd(MathHelper.lerp(1.0f, viewDistance, viewDistance / 3 + 0.0f));
+				break;
+			}
 		}
 	}
 
