6ec32e868d1f26538059f55e6b91e1eae9a416af
nvb-uy <almhak@proton.me>
2023-06-25 08:42:40 -0300

    temp(sign)

--- src/main/java/elocindev/eldritch_end/registry/BlockRegistry.java
+++ src/main/java/elocindev/eldritch_end/registry/BlockRegistry.java
@@ -8,9 +8,12 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.PillarBlock;
+import net.minecraft.block.SignBlock;
+import net.minecraft.block.WallSignBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.SignType;
 import net.minecraft.util.registry.Registry;
 
 public class BlockRegistry {
@@ -26,19 +29,34 @@ public class BlockRegistry {
 
     public static final BlockItem PRIMORDIAL_LOG_ITEM = new BlockItem(PRIMORDIAL_LOG, new FabricItemSettings());
 
-    public static void registerFullBlock(String identifier, Block blockInstance, BlockItem itemInstance) {
-        Registry.register(Registry.BLOCK, new Identifier(EldritchEnd.MODID, identifier), blockInstance);
-        ItemRegistry.reg(itemInstance, identifier);
-    }
+    public static final Block PRIMORDIAL_PLANKS = new Block(FabricBlockSettings
+        .copyOf(Blocks.OAK_PLANKS)
+        .sounds(BlockSoundGroup.WOOD));
+    public static final BlockItem PRIMORDIAL_PLANKS_ITEM = new BlockItem(PRIMORDIAL_PLANKS, new FabricItemSettings());
+
+    public static final Block PRIMORDIAL_SIGN = new SignBlock(FabricBlockSettings
+        .copyOf(Blocks.OAK_SIGN)
+        .sounds(BlockSoundGroup.WOOD), SignType.OAK);
+    public static final Block PRIMORDIAL_WALL_SIGN = new WallSignBlock(null, null);
+
+    public static final BlockItem PRIMORDIAL_SIGN_ITEM = new BlockItem(PRIMORDIAL_SIGN, new FabricItemSettings());
+
 
     public static void register() {
         registerFullBlock("abysmal_fronds", ABYSMAL_FRONDS, ABYSMAL_FRONDS_ITEM);
         registerFullBlock("primordial_log", PRIMORDIAL_LOG, PRIMORDIAL_LOG_ITEM);
         registerFullBlock("suspicious_fronds", SUSPICIOUS_FRONDS, SUSPICIOUS_FRONDS_ITEM);
+        registerFullBlock("primordial_planks", PRIMORDIAL_PLANKS, PRIMORDIAL_PLANKS_ITEM);
+        registerFullBlock("primordial_sign", PRIMORDIAL_SIGN, PRIMORDIAL_SIGN_ITEM);
 
         // Grass Overhaul Compatibility
         // if (FabricLoader.getInstance().isModLoaded("grassoverhaul") || FabricLoader.getInstance().isModLoaded("sod")) {
         //     // todo later
         // }
+    }
+
+    public static void registerFullBlock(String identifier, Block blockInstance, BlockItem itemInstance) {
+        Registry.register(Registry.BLOCK, new Identifier(EldritchEnd.MODID, identifier), blockInstance);
+        ItemRegistry.reg(itemInstance, identifier);
     }
 }
