42511d5b4449b837f06416d9396c3c0ce60f18b9
nvb-uy <almhak@proton.me>
2023-08-10 19:53:28 -0300

    temp(Corruption draft)

---
+++ src/main/java/elocindev/eldritch_end/effects/CorruptionEffect.java
@@ -0,0 +1,13 @@
+package elocindev.eldritch_end.effects;
+
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+
+public class CorruptionEffect extends StatusEffect {
+
+    protected CorruptionEffect(StatusEffectCategory statusEffectCategory, int color) {
+        super(statusEffectCategory, color);
+    }
+    
+    
+}
