449a588077ada02fbd97a542e741735c982201b4
nvb-uy <almhak@proton.me>
2023-08-10 16:02:44 -0300

    add(Aberration attack applies stacking effect)

--- src/main/java/elocindev/eldritch_end/entity/aberration/AberrationEntity.java
+++ src/main/java/elocindev/eldritch_end/entity/aberration/AberrationEntity.java
@@ -1,12 +1,18 @@
 package elocindev.eldritch_end.entity.aberration;
 
 import elocindev.eldritch_end.config.Configs;
+import elocindev.eldritch_end.registry.BlockRegistry;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -34,7 +40,29 @@ public class AberrationEntity extends HostileEntity implements IAnimatable {
         this.targetSelector.add(2, new ActiveTargetGoal<>(this, PlayerEntity.class, true));
     }
 
-    // Todo: replace with desired stats
+    @Override
+    public void tickMovement() {
+        super.tickMovement();
+
+        if (this.age % 20 == 0 && this.world.getBlockState(new BlockPos(this.getX(), this.getY() - 1, this.getZ())).getBlock() == BlockRegistry.ABYSMAL_FRONDS) {
+            this.heal(this.getMaxHealth() / 10);
+        }
+    }
+
+    @Override
+    public boolean tryAttack(Entity target) {
+        if (target instanceof PlayerEntity victim) {
+            StatusEffect effect = StatusEffects.SLOWNESS; // TODO: Replace with corruption effect
+
+            if (victim.hasStatusEffect(effect))
+                victim.addStatusEffect(new StatusEffectInstance(effect, victim.getStatusEffect(effect).getDuration(), victim.getStatusEffect(effect).getAmplifier() + 1));
+            else
+                victim.addStatusEffect(new StatusEffectInstance(effect, 200, 0));
+        }
+
+        return super.tryAttack(target);
+    }
+
     public static DefaultAttributeContainer.Builder setAttributes() {
         return HostileEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, Configs.ENTITY_ABERRATION.HEALTH)
@@ -68,7 +96,7 @@ public class AberrationEntity extends HostileEntity implements IAnimatable {
             event.getController().markNeedsReload();
             event.getController().setAnimation(new AnimationBuilder().addAnimation("attack", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
         }
-
+        
         return PlayState.CONTINUE;
     }
 
