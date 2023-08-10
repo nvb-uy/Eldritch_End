2d46789d3c2690e25f0f112daeedf38aea5e7ba4
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-08-10 10:19:31 +0100

    fix(spawn egg color)

--- src/main/java/elocindev/eldritch_end/registry/ItemRegistry.java
+++ src/main/java/elocindev/eldritch_end/registry/ItemRegistry.java
@@ -15,7 +15,7 @@ import elocindev.eldritch_end.EldritchEnd;
 public class ItemRegistry {
     private static final Logger LOGGER = LoggerFactory.getLogger(EldritchEnd.MODID);
 
-    public static final Item ABERRATION_SPAWN_EGG = new SpawnEggItem(EntityRegistry.ABERRATION, 0x54496f, 0x3e3652, new FabricItemSettings());
+    public static final Item ABERRATION_SPAWN_EGG = new SpawnEggItem(EntityRegistry.ABERRATION, 0x54496f, 0x726f76, new FabricItemSettings());
 
     public static Item reg(Item instance, String id) {
         return Registry.register(Registry.ITEM, new Identifier(EldritchEnd.MODID, id), instance);
