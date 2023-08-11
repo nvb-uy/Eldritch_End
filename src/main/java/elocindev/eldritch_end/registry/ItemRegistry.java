7b2a6851db713c84891fe49887ca730f862671f1
nvb-uy <almhak@proton.me>
2023-08-10 21:53:30 -0300

    fix(unused import warning)

--- src/main/java/elocindev/eldritch_end/registry/ItemRegistry.java
+++ src/main/java/elocindev/eldritch_end/registry/ItemRegistry.java
@@ -2,7 +2,6 @@ package elocindev.eldritch_end.registry;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.SpawnEggItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
