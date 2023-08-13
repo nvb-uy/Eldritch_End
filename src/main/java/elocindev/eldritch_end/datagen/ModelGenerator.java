cc1b8713852209cdafa8bf60a28abfd0f92bebba
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-08-13 05:35:39 +0100

    add(fronds generation method)

--- src/main/java/elocindev/eldritch_end/datagen/ModelGenerator.java
+++ src/main/java/elocindev/eldritch_end/datagen/ModelGenerator.java
@@ -6,10 +6,12 @@ import elocindev.eldritch_end.registry.ItemRegistry;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.enums.SlabType;
 import net.minecraft.data.client.*;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Direction;
 
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
@@ -25,7 +27,8 @@ public class ModelGenerator extends FabricModelProvider {
         generator.registerSimpleCubeAll(BlockRegistry.PRIMORDIAL_PLANKS);
         generator.registerDoor(BlockRegistry.PRIMORDIAL_DOOR);
         generator.registerTrapdoor(BlockRegistry.PRIMORDIAL_TRAPDOOR);
-        
+
+        registerFronds(BlockRegistry.ABYSMAL_FRONDS, generator.modelCollector, generator.blockStateCollector);
         registerSlab(BlockRegistry.PRIMORDIAL_SLAB, BlockRegistry.PRIMORDIAL_PLANKS, generator.modelCollector, generator.blockStateCollector);
     }
 
@@ -45,6 +48,15 @@ public class ModelGenerator extends FabricModelProvider {
         Identifier identifier3 = Models.CUBE_COLUMN.uploadWithoutVariant(slabBlock, "_double", textureMap2, modelCollector);
 
         blockStateCollector.accept(createSlabBlockState(slabBlock, identifier, identifier2, identifier3));
+    }
+
+    public final void registerFronds(Block block, BiConsumer<Identifier, Supplier<JsonElement>> modelCollector, Consumer<BlockStateSupplier> blockStateCollector) {
+        TextureMap textureMap = (new TextureMap()).put(TextureKey.BOTTOM, TextureMap.getId(Blocks.END_STONE)).put(TextureKey.TOP, TextureMap.getSubId(block, "_top")).put(TextureKey.SIDE, TextureMap.getSubId(block, ""));
+        blockStateCollector.accept(createSingletonBlockState(block, Models.CUBE_BOTTOM_TOP.upload(block, textureMap, modelCollector)));
+    }
+
+    public static VariantsBlockStateSupplier createSingletonBlockState(Block block, Identifier modelId) {
+        return VariantsBlockStateSupplier.create(block, BlockStateVariant.create().put(VariantSettings.MODEL, modelId));
     }
 
     public static BlockStateSupplier createSlabBlockState(Block slabBlock, Identifier bottomModelId, Identifier topModelId, Identifier fullModelId) {
