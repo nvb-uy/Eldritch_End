4b72b13f9740ae333fc12213fa4b2c804c88c490
nvb-uy <almhak@proton.me>
2023-08-14 15:57:25 -0300

    feat(Chorb easter egg)

---
+++ src/main/java/elocindev/eldritch_end/item/Chorb.java
@@ -0,0 +1,41 @@
+package elocindev.eldritch_end.item;
+
+import java.util.List;
+
+import org.jetbrains.annotations.Nullable;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SpawnEggItem;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
+
+public class Chorb extends SpawnEggItem {
+    
+    public Chorb(EntityType<? extends MobEntity> type, int primaryColor, int secondaryColor, Settings settings) {
+        super(type, primaryColor, secondaryColor, settings);
+    }
+    
+    String[] TOOLTIP_NORMAL = {
+        "",
+        "\u00A7l\u2022 \u00A76Chorbin' Time \u00A78(Basic Attack)",
+        "\u00A77Chorb chorbs at the target, dealing \u00A7d93041 emotional damage\u00A77.",
+        "",
+        "\u00A7l\u2022 \u00A76Bladee's Blessing \u00A78(Active, 30s Cooldown)",
+        "\u00A77Chorb starts listening to \u00A76BLADEE - WASTER\u00A77, increasing his damage 25% for the song's duration.",
+        "",
+        "\u00A7l\u2022 \u00A76Shrimp in the shower \u00A78(Ultimate, 120s Cooldown)",
+        "\u00A77Chorb spawns a shower and starts eating shrimp for 10s,",
+        "\u00A77healing \u00A7610%\u00A77 max health per second."
+    };
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        for (String line : TOOLTIP_NORMAL) {
+            tooltip.add(Text.of(line));
+        }
+    }
+
+}
--- src/main/java/elocindev/eldritch_end/registry/ItemRegistry.java
+++ src/main/java/elocindev/eldritch_end/registry/ItemRegistry.java
@@ -1,6 +1,7 @@
 package elocindev.eldritch_end.registry;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -9,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import elocindev.eldritch_end.EldritchEnd;
+import elocindev.eldritch_end.item.Chorb;
 import elocindev.eldritch_end.item.Necronomicon;
 import elocindev.eldritch_end.item.SilverKey;
 import elocindev.eldritch_end.item.spawneggs.AberrationEgg;
@@ -18,6 +20,8 @@ public class ItemRegistry {
 
     public static final Item ABERRATION_SPAWN_EGG = new AberrationEgg(EntityRegistry.ABERRATION, 0x54496f, 0x726f76, new FabricItemSettings());
     
+    public static final Item CHORB_SPAWN_EGG = new Chorb(EntityType.GIANT, 0xfcba03, 0xfce703, new FabricItemSettings());
+
     public static final Item NECRONOMICON = new Necronomicon(new FabricItemSettings());
     public static final Item SILVER_KEY = new SilverKey(new FabricItemSettings());
 
@@ -28,6 +32,7 @@ public class ItemRegistry {
     public static void register() {
         
         reg(ABERRATION_SPAWN_EGG, "aberration_spawn_egg");
+        reg(CHORB_SPAWN_EGG, "chorb_spawn_egg");
         reg(NECRONOMICON, "necronomicon");
         reg(SILVER_KEY, "silver_key");
         LOGGER.info("Registered Eldritch End's items");
