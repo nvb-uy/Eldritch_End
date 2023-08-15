1119f15f0b2a5e4b3518709a1b568c6d1c8c8913
nvb-uy <almhak@proton.me>
2023-08-14 23:50:26 -0300

    fix(tentacle tooltip)

--- src/main/java/elocindev/eldritch_end/item/spawneggs/TentacleEgg.java
+++ src/main/java/elocindev/eldritch_end/item/spawneggs/TentacleEgg.java
@@ -20,12 +20,11 @@ public class TentacleEgg extends SpawnEggItem {
     
     String[] TOOLTIP_NORMAL = {
         "",
-        "\\u00A7l\\u2022 \u00A79Swirl \u00A78(Basic Attack)",
-        "\u00A77Damages all creatures in a 2 block radius for\u00A79"+ (int)Configs.ENTITY_ABERRATION.ATTACK_DAMAGE_ATTRIBUTE + "\u00A77 damage.",
-        "\u00A77Applies \u00A79\u00A7nCorruption\u00A7r\u00A77 for "+Configs.ENTITY_ABERRATION.initital_corruption_duration_ticks/20+"s. Stacks don't reset its duration." ,
+        "\u00A7l\u2022 \u00A79Swirl \u00A78(Basic Attack)",
+        "\u00A77Damages all creatures in a 2 block radius for \u00A79"+ (int)Configs.ENTITY_ABERRATION.ATTACK_DAMAGE_ATTRIBUTE + "\u00A77 damage.",
         "",
-        "\\u00A7l\\u2022 \u00A79Azathoth's Gift \u00A78(Passive)",
-        "\u00A77Immune to \u00A79corruption\u00A77 damage."
+        "\u00A7l\u2022 \u00A79Azathoth's Gift \u00A78(Passive)",
+        "\u00A77Grants immunity to \u00A79corruption\u00A77 damage."
     };
 
     @Override
