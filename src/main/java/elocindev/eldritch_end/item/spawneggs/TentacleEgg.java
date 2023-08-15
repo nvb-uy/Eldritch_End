93d79934e6f093f6f3a8c9212b1e038cf2cfc6a7
nvb-uy <almhak@proton.me>
2023-08-14 23:12:48 -0300

    fix(Tentacle Tooltip)

--- src/main/java/elocindev/eldritch_end/item/spawneggs/TentacleEgg.java
+++ src/main/java/elocindev/eldritch_end/item/spawneggs/TentacleEgg.java
@@ -20,12 +20,12 @@ public class TentacleEgg extends SpawnEggItem {
     
     String[] TOOLTIP_NORMAL = {
         "",
-        "\u00A79Bite \u00A78(Basic Attack)",
-        "\u00A77Bites the target and deals \u00A7c"+ (int)Configs.ENTITY_ABERRATION.ATTACK_DAMAGE_ATTRIBUTE + "\u00A77 + \u00A7925% \u00A77per \u00A7ncorruption stack\u00A77.",
+        "\u00A79Swirl \u00A78(Basic Attack)",
+        "\u00A77Damages all creatures in a 2 block radius for\u00A79"+ (int)Configs.ENTITY_ABERRATION.ATTACK_DAMAGE_ATTRIBUTE + "\u00A77 damage.",
         "\u00A77Applies \u00A79\u00A7nCorruption\u00A7r\u00A77 for "+Configs.ENTITY_ABERRATION.initital_corruption_duration_ticks/20+"s. Stacks don't reset its duration." ,
         "",
-        "\u00A79Shadow's Grasp \u00A78(Passive, 1s Cooldown)",
-        "\u00A77Heals 10% of max health if walking on Abysmal Fronds."
+        "\u00A79Azathoth's Gift \u00A78(Passive)",
+        "\u00A77Immune to \u00A79corruption\u00A77 damage."
     };
 
     @Override
