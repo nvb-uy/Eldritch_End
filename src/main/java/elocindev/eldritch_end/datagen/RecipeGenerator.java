477991f2f47755ba74b8384bffb1e2449700f9c5
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-08-16 04:41:57 +0100

    add(recipe datagen)

---
+++ src/main/java/elocindev/eldritch_end/datagen/RecipeGenerator.java
@@ -0,0 +1,25 @@
+package elocindev.eldritch_end.datagen;
+
+import elocindev.eldritch_end.EldritchEnd;
+import elocindev.eldritch_end.registry.BlockRegistry;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.block.Blocks;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.tag.ItemTags;
+import net.minecraft.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import java.util.function.Consumer;
+
+public class RecipeGenerator extends FabricRecipeProvider {
+    public RecipeGenerator(FabricDataGenerator dataGenerator) {
+        super(dataGenerator);
+    }
+
+    @Override
+    protected void generateRecipes(Consumer<RecipeJsonProvider> exporter) {
+        offerPlanksRecipe(exporter, Blocks.ACACIA_PLANKS, TagKey.of(Registry.ITEM_KEY, new Identifier(EldritchEnd.MODID, "primordial_log")));
+    }
+}
