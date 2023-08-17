263d815db872a0ca4c5d317847e1f82618761499
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-08-17 07:54:20 +0100

    add(datagenned tag)

--- src/main/java/elocindev/eldritch_end/datagen/TagGenerator.java
+++ src/main/java/elocindev/eldritch_end/datagen/TagGenerator.java
@@ -1,15 +1,24 @@
 package elocindev.eldritch_end.datagen;
 
+import elocindev.eldritch_end.EldritchEnd;
+import elocindev.eldritch_end.registry.BlockRegistry;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.block.Block;
+import net.minecraft.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class TagGenerator extends FabricTagProvider.BlockTagProvider {
     public TagGenerator(FabricDataGenerator dataGenerator) {
         super(dataGenerator);
     }
 
+    private static final TagKey<Block> PRIMORDIAL_LOGS = TagKey.of(Registry.BLOCK_KEY, new Identifier(EldritchEnd.MODID, "primordial_logs"));
+
     @Override
     protected void generateTags() {
-
+        getOrCreateTagBuilder(PRIMORDIAL_LOGS)
+                .add(BlockRegistry.PRIMORDIAL_LOG);
     }
 }
