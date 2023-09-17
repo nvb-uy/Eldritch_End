ec9e56f3be22e066a3d98d973857a60c0a3cff70
nvb-uy <105564560+nvb-uy@users.noreply.github.com>
2023-09-17 18:28:29 -0300

    add(tooltip to etyrite pieces)

---
+++ src/main/java/elocindev/eldritch_end/item/armor/EtyriteArmorPiece.java
@@ -0,0 +1,31 @@
+package elocindev.eldritch_end.item.armor;
+
+import java.util.List;
+
+import org.jetbrains.annotations.Nullable;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
+
+public class EtyriteArmorPiece extends ArmorItem {
+
+    public EtyriteArmorPiece(ArmorMaterial material, EquipmentSlot slot, Settings settings) {
+        super(material, slot, settings);
+    }
+
+    String[] TOOLTIP_NORMAL = {
+        "\u00A72+5 Corruption Resistance"
+    };
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        for (String line : TOOLTIP_NORMAL) {
+            tooltip.add(Text.of(line));
+        }
+    }    
+}
