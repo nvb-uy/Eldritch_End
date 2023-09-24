34087933a4732f0078499e6a958fe12337d6e436
nvb-uy <105564560+nvb-uy@users.noreply.github.com>
2023-09-24 02:57:38 -0300

    add(aberration's heart)

--- src/main/java/elocindev/eldritch_end/registry/ItemGroupRegistry.java
+++ src/main/java/elocindev/eldritch_end/registry/ItemGroupRegistry.java
@@ -11,7 +11,10 @@ public class ItemGroupRegistry {
         .icon(() -> new ItemStack(BlockRegistry.ABYSMAL_FRONDS_ITEM))
         .appendItems(stacks -> {
             stacks.add(new ItemStack(ItemRegistry.NECRONOMICON));
+
             stacks.add(new ItemStack(ItemRegistry.ABERRATION_LIMB));
+            stacks.add(new ItemStack(ItemRegistry.ABERRATION_HEART));
+
             stacks.add(new ItemStack(ItemRegistry.RAW_ETYR));
             stacks.add(new ItemStack(ItemRegistry.ETYR_INGOT));
             stacks.add(new ItemStack(ItemRegistry.ETYR_UPGRADE_PATTERN));
