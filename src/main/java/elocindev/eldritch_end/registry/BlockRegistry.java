bd9b18f06e2474e8b5789933d39405bed255ab2e
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-09-26 13:05:23 +0100

    fix(block registry class)

--- src/main/java/elocindev/eldritch_end/registry/BlockRegistry.java
+++ src/main/java/elocindev/eldritch_end/registry/BlockRegistry.java
@@ -15,9 +15,10 @@ import net.minecraft.block.AbstractBlock.Settings;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.SignItem;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class BlockRegistry {
     public static final Block ABYSMAL_FRONDS = new AbysmalFronds();
@@ -72,47 +73,41 @@ public class BlockRegistry {
     	public static final BlockItem PRIMORDIAL_SLAB_ITEM = new BlockItem(PRIMORDIAL_SLAB, new FabricItemSettings());
 
     public static Block PRIMORDIAL_PRESSURE_PLATE = new PressurePlateBlock(PressurePlateBlock.ActivationRule.EVERYTHING, FabricBlockSettings
-        .copyOf(Blocks.OAK_PRESSURE_PLATE)
-        .sounds(BlockSoundGroup.WOOD));
+        .copyOf(Blocks.OAK_PRESSURE_PLATE).sounds(BlockSoundGroup.WOOD), BlockSetType.OAK);
 
-    	public static final BlockItem PRIMORDIAL_PRESSURE_PLATE_ITEM = new BlockItem(PRIMORDIAL_PRESSURE_PLATE, new FabricItemSettings());
-
-    // 
+    public static final BlockItem PRIMORDIAL_PRESSURE_PLATE_ITEM = new BlockItem(PRIMORDIAL_PRESSURE_PLATE, new FabricItemSettings());
 
     public static final Block PRIMORDIAL_DOOR = new DoorBlock(FabricBlockSettings
-        .copyOf(Blocks.OAK_DOOR)
-        .sounds(BlockSoundGroup.WOOD));
+        .copyOf(Blocks.OAK_DOOR).sounds(BlockSoundGroup.WOOD), BlockSetType.OAK);
 
     public static final BlockItem PRIMORDIAL_DOOR_ITEM = new BlockItem(PRIMORDIAL_DOOR, new FabricItemSettings());
 
     public static final Block PRIMORDIAL_TRAPDOOR = new TrapdoorBlock(FabricBlockSettings
-        .copyOf(Blocks.OAK_TRAPDOOR)
-        .sounds(BlockSoundGroup.WOOD));
+        .copyOf(Blocks.OAK_TRAPDOOR).sounds(BlockSoundGroup.WOOD), BlockSetType.OAK);
 
     public static final BlockItem PRIMORDIAL_TRAPDOOR_ITEM = new BlockItem(PRIMORDIAL_TRAPDOOR, new FabricItemSettings());
 
-    public static final SignBlock PRIMORDIAL_SIGN = new SignBlock(FabricBlockSettings
-        .copyOf(Blocks.OAK_SIGN)
-        .sounds(BlockSoundGroup.WOOD), EldritchProperties.PRIMORDIAL_SIGN_TYPE);
-    
-    public static final WallSignBlock PRIMORDIAL_WALL_SIGN = new WallSignBlock(Settings.copy(PRIMORDIAL_SIGN), EldritchProperties.PRIMORDIAL_SIGN_TYPE);
+    // Todo: Change this to EldritchProperties.PRIMORDIAL_SIGN_TYPE
+    public static final SignBlock PRIMORDIAL_SIGN = new SignBlock(FabricBlockSettings.copyOf(Blocks.OAK_SIGN).sounds(BlockSoundGroup.WOOD), WoodType.OAK);
 
+    // Todo: Change this to EldritchProperties.PRIMORDIAL_SIGN_TYPE
+    public static final WallSignBlock PRIMORDIAL_WALL_SIGN = new WallSignBlock(Settings.copy(PRIMORDIAL_SIGN), WoodType.OAK);
     public static final BlockItem PRIMORDIAL_SIGN_ITEM = new SignItem(new Item.Settings().maxCount(16), PRIMORDIAL_SIGN, PRIMORDIAL_WALL_SIGN);
-
 
     public static final StairsBlock PRIMORDIAL_STAIRS = new StairsBlock(PRIMORDIAL_PLANKS.getDefaultState(), FabricBlockSettings.copyOf(Blocks.OAK_STAIRS));
     public static final BlockItem PRIMORDIAL_STAIRS_ITEM = new BlockItem(PRIMORDIAL_STAIRS, new FabricItemSettings());
 
-    public static final WoodenButtonBlock PRIMORDIAL_BUTTON = new WoodenButtonBlock(FabricBlockSettings.copyOf(Blocks.OAK_STAIRS));
+    public static final ButtonBlock PRIMORDIAL_BUTTON = new ButtonBlock(FabricBlockSettings.copyOf(Blocks.OAK_WOOD), BlockSetType.OAK, 30, true);
     public static final BlockItem PRIMORDIAL_BUTTON_ITEM = new BlockItem(PRIMORDIAL_BUTTON, new FabricItemSettings());
 
     public static final Block HASTURIAN_MOSS = new HasturianMoss();
     public static final BlockItem HASTURIAN_MOSS_ITEM = new BlockItem(HASTURIAN_MOSS, new FabricItemSettings());
 
-    public static final Block PRIMORDIAL_FENCE = new FenceBlock(FabricBlockSettings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD));
+    public static final Block PRIMORDIAL_FENCE = new FenceBlock(FabricBlockSettings.copyOf(Blocks.OAK_FENCE).sounds(BlockSoundGroup.WOOD));
     public static final BlockItem PRIMORDIAL_FENCE_ITEM = new BlockItem(PRIMORDIAL_FENCE, new FabricItemSettings());
 
-    public static final Block PRIMORDIAL_FENCE_GATE = new FenceGateBlock(FabricBlockSettings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD));
+    // Todo: change to primordial wood
+    public static final Block PRIMORDIAL_FENCE_GATE = new FenceGateBlock(FabricBlockSettings.copyOf(Blocks.OAK_FENCE_GATE).sounds(BlockSoundGroup.WOOD), WoodType.OAK);
     public static final BlockItem PRIMORDIAL_FENCE_GATE_ITEM = new BlockItem(PRIMORDIAL_FENCE_GATE, new FabricItemSettings());
 
     public static final Block HASTURIAN_DUNE_SAND = new FallingBlock(FabricBlockSettings.copyOf(Blocks.SAND));
@@ -128,7 +123,7 @@ public class BlockRegistry {
     public static final BlockItem HASTURIAN_GRASS_ITEM = new BlockItem(HASTURIAN_GRASS, new FabricItemSettings());
 
     // Default etyr
-    public static final Block ETYR_ORE = new OreBlock(FabricBlockSettings.copyOf(Blocks.IRON_ORE));
+    public static final Block ETYR_ORE = new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.IRON_ORE));
     public static final BlockItem ETYR_ORE_ITEM = new BlockItem(ETYR_ORE, new FabricItemSettings());
 
     public static final Block ETYR_BLOCK = new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK));
@@ -137,9 +132,7 @@ public class BlockRegistry {
     public static final Block ETYR_BARS = new PaneBlock(FabricBlockSettings.copyOf(Blocks.IRON_BARS));
     public static final BlockItem ETYR_BARS_ITEM = new BlockItem(ETYR_BARS, new FabricItemSettings());
 
-    public static final Block ETYR_DOOR = new DoorBlock(FabricBlockSettings
-            .copyOf(Blocks.IRON_DOOR)
-            .sounds(BlockSoundGroup.METAL));
+    public static final Block ETYR_DOOR = new DoorBlock(FabricBlockSettings.copyOf(Blocks.IRON_DOOR).sounds(BlockSoundGroup.METAL), BlockSetType.OAK);
     public static final BlockItem ETYR_DOOR_ITEM = new BlockItem(ETYR_DOOR, new FabricItemSettings());
 
     public static final Block ETYR_PILLAR = new PillarBlock(FabricBlockSettings.copyOf(Blocks.QUARTZ_PILLAR));
@@ -148,16 +141,10 @@ public class BlockRegistry {
     public static final Block ETYR_TILES = new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK));
     public static final BlockItem ETYR_TILES_ITEM = new BlockItem(ETYR_TILES, new FabricItemSettings());
 
-    public static final Block ETYR_TRAPDOOR = new TrapdoorBlock(FabricBlockSettings
-            .copyOf(Blocks.IRON_TRAPDOOR)
-            .sounds(BlockSoundGroup.METAL));
-
+    public static final Block ETYR_TRAPDOOR = new TrapdoorBlock(FabricBlockSettings.copyOf(Blocks.IRON_TRAPDOOR).sounds(BlockSoundGroup.METAL), BlockSetType.IRON);
     public static final BlockItem ETYR_TRAPDOOR_ITEM = new BlockItem(ETYR_TRAPDOOR, new FabricItemSettings());
 
-    public static final Block ETYR_SLAB = new SlabBlock(FabricBlockSettings
-            .copyOf(Blocks.STONE_SLAB)
-            .sounds(BlockSoundGroup.METAL));
-
+    public static final Block ETYR_SLAB = new SlabBlock(FabricBlockSettings.copyOf(Blocks.STONE_SLAB).sounds(BlockSoundGroup.METAL));
     public static final BlockItem ETYR_SLAB_ITEM = new BlockItem(ETYR_SLAB, new FabricItemSettings());
 
     public static final StairsBlock ETYR_STAIRS = new StairsBlock(ETYR_TILES.getDefaultState(), FabricBlockSettings.copyOf(Blocks.STONE_STAIRS));
@@ -171,9 +158,7 @@ public class BlockRegistry {
     public static final Block DECADENT_ETYR_BARS = new PaneBlock(FabricBlockSettings.copyOf(Blocks.IRON_BARS));
     public static final BlockItem DECADENT_ETYR_BARS_ITEM = new BlockItem(DECADENT_ETYR_BARS, new FabricItemSettings());
 
-    public static final Block DECADENT_ETYR_DOOR = new DoorBlock(FabricBlockSettings
-            .copyOf(Blocks.IRON_DOOR)
-            .sounds(BlockSoundGroup.METAL));
+    public static final Block DECADENT_ETYR_DOOR = new DoorBlock(FabricBlockSettings.copyOf(Blocks.IRON_DOOR).sounds(BlockSoundGroup.METAL), BlockSetType.IRON);
     public static final BlockItem DECADENT_ETYR_DOOR_ITEM = new BlockItem(DECADENT_ETYR_DOOR, new FabricItemSettings());
 
     public static final Block DECADENT_ETYR_PILLAR = new PillarBlock(FabricBlockSettings.copyOf(Blocks.QUARTZ_PILLAR));
@@ -182,16 +167,10 @@ public class BlockRegistry {
     public static final Block DECADENT_ETYR_TILES = new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK));
     public static final BlockItem DECADENT_ETYR_TILES_ITEM = new BlockItem(DECADENT_ETYR_TILES, new FabricItemSettings());
 
-    public static final Block DECADENT_ETYR_TRAPDOOR = new TrapdoorBlock(FabricBlockSettings
-            .copyOf(Blocks.IRON_TRAPDOOR)
-            .sounds(BlockSoundGroup.METAL));
-
+    public static final Block DECADENT_ETYR_TRAPDOOR = new TrapdoorBlock(FabricBlockSettings.copyOf(Blocks.IRON_TRAPDOOR).sounds(BlockSoundGroup.METAL), BlockSetType.IRON);
     public static final BlockItem DECADENT_ETYR_TRAPDOOR_ITEM = new BlockItem(DECADENT_ETYR_TRAPDOOR, new FabricItemSettings());
 
-    public static final Block DECADENT_ETYR_SLAB = new SlabBlock(FabricBlockSettings
-            .copyOf(Blocks.STONE_SLAB)
-            .sounds(BlockSoundGroup.METAL));
-
+    public static final Block DECADENT_ETYR_SLAB = new SlabBlock(FabricBlockSettings.copyOf(Blocks.STONE_SLAB).sounds(BlockSoundGroup.METAL));
     public static final BlockItem DECADENT_ETYR_SLAB_ITEM = new BlockItem(DECADENT_ETYR_SLAB, new FabricItemSettings());
 
     public static final StairsBlock DECADENT_ETYR_STAIRS = new StairsBlock(DECADENT_ETYR_TILES.getDefaultState(), FabricBlockSettings.copyOf(Blocks.STONE_STAIRS));
@@ -205,9 +184,7 @@ public class BlockRegistry {
     public static final Block PERTURBED_ETYR_BARS = new PaneBlock(FabricBlockSettings.copyOf(Blocks.IRON_BARS));
     public static final BlockItem PERTURBED_ETYR_BARS_ITEM = new BlockItem(PERTURBED_ETYR_BARS, new FabricItemSettings());
 
-    public static final Block PERTURBED_ETYR_DOOR = new DoorBlock(FabricBlockSettings
-            .copyOf(Blocks.IRON_DOOR)
-            .sounds(BlockSoundGroup.METAL));
+    public static final Block PERTURBED_ETYR_DOOR = new DoorBlock(FabricBlockSettings.copyOf(Blocks.IRON_DOOR).sounds(BlockSoundGroup.METAL), BlockSetType.IRON);
     public static final BlockItem PERTURBED_ETYR_DOOR_ITEM = new BlockItem(PERTURBED_ETYR_DOOR, new FabricItemSettings());
 
     public static final Block PERTURBED_ETYR_PILLAR = new PillarBlock(FabricBlockSettings.copyOf(Blocks.QUARTZ_PILLAR));
@@ -216,16 +193,10 @@ public class BlockRegistry {
     public static final Block PERTURBED_ETYR_TILES = new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK));
     public static final BlockItem PERTURBED_ETYR_TILES_ITEM = new BlockItem(PERTURBED_ETYR_TILES, new FabricItemSettings());
 
-    public static final Block PERTURBED_ETYR_TRAPDOOR = new TrapdoorBlock(FabricBlockSettings
-            .copyOf(Blocks.IRON_TRAPDOOR)
-            .sounds(BlockSoundGroup.METAL));
-
+    public static final Block PERTURBED_ETYR_TRAPDOOR = new TrapdoorBlock(FabricBlockSettings.copyOf(Blocks.IRON_TRAPDOOR).sounds(BlockSoundGroup.METAL), BlockSetType.IRON);
     public static final BlockItem PERTURBED_ETYR_TRAPDOOR_ITEM = new BlockItem(PERTURBED_ETYR_TRAPDOOR, new FabricItemSettings());
 
-    public static final Block PERTURBED_ETYR_SLAB = new SlabBlock(FabricBlockSettings
-            .copyOf(Blocks.STONE_SLAB)
-            .sounds(BlockSoundGroup.METAL));
-
+    public static final Block PERTURBED_ETYR_SLAB = new SlabBlock(FabricBlockSettings.copyOf(Blocks.STONE_SLAB).sounds(BlockSoundGroup.METAL));
     public static final BlockItem PERTURBED_ETYR_SLAB_ITEM = new BlockItem(PERTURBED_ETYR_SLAB, new FabricItemSettings());
 
     public static final StairsBlock PERTURBED_ETYR_STAIRS = new StairsBlock(PERTURBED_ETYR_TILES.getDefaultState(), FabricBlockSettings.copyOf(Blocks.STONE_STAIRS));
@@ -239,9 +210,7 @@ public class BlockRegistry {
     public static final Block CORRUPTED_ETYR_BARS = new PaneBlock(FabricBlockSettings.copyOf(Blocks.IRON_BARS));
     public static final BlockItem CORRUPTED_ETYR_BARS_ITEM = new BlockItem(CORRUPTED_ETYR_BARS, new FabricItemSettings());
 
-    public static final Block CORRUPTED_ETYR_DOOR = new DoorBlock(FabricBlockSettings
-            .copyOf(Blocks.IRON_DOOR)
-            .sounds(BlockSoundGroup.METAL));
+    public static final Block CORRUPTED_ETYR_DOOR = new DoorBlock(FabricBlockSettings.copyOf(Blocks.IRON_DOOR).sounds(BlockSoundGroup.METAL), BlockSetType.IRON);
     public static final BlockItem CORRUPTED_ETYR_DOOR_ITEM = new BlockItem(CORRUPTED_ETYR_DOOR, new FabricItemSettings());
 
     public static final Block CORRUPTED_ETYR_PILLAR = new PillarBlock(FabricBlockSettings.copyOf(Blocks.QUARTZ_PILLAR));
@@ -250,25 +219,17 @@ public class BlockRegistry {
     public static final Block CORRUPTED_ETYR_TILES = new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK));
     public static final BlockItem CORRUPTED_ETYR_TILES_ITEM = new BlockItem(CORRUPTED_ETYR_TILES, new FabricItemSettings());
 
-    public static final Block CORRUPTED_ETYR_TRAPDOOR = new TrapdoorBlock(FabricBlockSettings
-            .copyOf(Blocks.IRON_TRAPDOOR)
-            .sounds(BlockSoundGroup.METAL));
-
+    public static final Block CORRUPTED_ETYR_TRAPDOOR = new TrapdoorBlock(FabricBlockSettings.copyOf(Blocks.IRON_TRAPDOOR).sounds(BlockSoundGroup.METAL), BlockSetType.IRON);
     public static final BlockItem CORRUPTED_ETYR_TRAPDOOR_ITEM = new BlockItem(CORRUPTED_ETYR_TRAPDOOR, new FabricItemSettings());
 
-    public static final Block CORRUPTED_ETYR_SLAB = new SlabBlock(FabricBlockSettings
-            .copyOf(Blocks.STONE_SLAB)
-            .sounds(BlockSoundGroup.METAL));
-
+    public static final Block CORRUPTED_ETYR_SLAB = new SlabBlock(FabricBlockSettings.copyOf(Blocks.STONE_SLAB).sounds(BlockSoundGroup.METAL));
     public static final BlockItem CORRUPTED_ETYR_SLAB_ITEM = new BlockItem(CORRUPTED_ETYR_SLAB, new FabricItemSettings());
 
     public static final StairsBlock CORRUPTED_ETYR_STAIRS = new StairsBlock(CORRUPTED_ETYR_TILES.getDefaultState(), FabricBlockSettings.copyOf(Blocks.STONE_STAIRS));
     public static final BlockItem CORRUPTED_ETYR_STAIRS_ITEM = new BlockItem(CORRUPTED_ETYR_STAIRS, new FabricItemSettings());
 
     // Spire stone + variants
-    public static final Block SPIRE_STONE = new Block(FabricBlockSettings
-            .copyOf(Blocks.STONE)
-            .sounds(BlockSoundGroup.STONE));
+    public static final Block SPIRE_STONE = new Block(FabricBlockSettings.copyOf(Blocks.STONE).sounds(BlockSoundGroup.STONE));
     public static final BlockItem SPIRE_STONE_ITEM = new BlockItem(SPIRE_STONE, new FabricItemSettings());
 
     public static final Block POLISHED_SPIRE_STONE = new Block(FabricBlockSettings.copyOf(Blocks.STONE));
@@ -293,24 +254,16 @@ public class BlockRegistry {
     public static final BlockItem CRACKED_POLISHED_SPIRE_STONE_TILES_ITEM = new BlockItem(CRACKED_POLISHED_SPIRE_STONE_TILES, new FabricItemSettings());
 
     // Slabs
-    public static final Block SPIRE_STONE_SLAB = new SlabBlock(FabricBlockSettings
-            .copyOf(Blocks.STONE_SLAB)
-            .sounds(BlockSoundGroup.STONE));
+    public static final Block SPIRE_STONE_SLAB = new SlabBlock(FabricBlockSettings.copyOf(Blocks.STONE_SLAB).sounds(BlockSoundGroup.STONE));
     public static final BlockItem SPIRE_STONE_SLAB_ITEM = new BlockItem(SPIRE_STONE_SLAB, new FabricItemSettings());
 
-    public static final Block POLISHED_SPIRE_STONE_SLAB = new SlabBlock(FabricBlockSettings
-            .copyOf(Blocks.STONE_SLAB)
-            .sounds(BlockSoundGroup.STONE));
+    public static final Block POLISHED_SPIRE_STONE_SLAB = new SlabBlock(FabricBlockSettings.copyOf(Blocks.STONE_SLAB).sounds(BlockSoundGroup.STONE));
     public static final BlockItem POLISHED_SPIRE_STONE_SLAB_ITEM = new BlockItem(POLISHED_SPIRE_STONE_SLAB, new FabricItemSettings());
 
-    public static final Block POLISHED_SPIRE_STONE_BRICK_SLAB = new SlabBlock(FabricBlockSettings
-            .copyOf(Blocks.STONE_SLAB)
-            .sounds(BlockSoundGroup.STONE));
+    public static final Block POLISHED_SPIRE_STONE_BRICK_SLAB = new SlabBlock(FabricBlockSettings.copyOf(Blocks.STONE_SLAB).sounds(BlockSoundGroup.STONE));
     public static final BlockItem POLISHED_SPIRE_STONE_BRICK_SLAB_ITEM = new BlockItem(POLISHED_SPIRE_STONE_BRICK_SLAB, new FabricItemSettings());
 
-    public static final Block POLISHED_SPIRE_STONE_TILE_SLAB = new SlabBlock(FabricBlockSettings
-            .copyOf(Blocks.STONE_SLAB)
-            .sounds(BlockSoundGroup.STONE));
+    public static final Block POLISHED_SPIRE_STONE_TILE_SLAB = new SlabBlock(FabricBlockSettings.copyOf(Blocks.STONE_SLAB).sounds(BlockSoundGroup.STONE));
     public static final BlockItem POLISHED_SPIRE_STONE_TILE_SLAB_ITEM = new BlockItem(POLISHED_SPIRE_STONE_TILE_SLAB, new FabricItemSettings());
 
     // Stairs
@@ -415,13 +368,13 @@ public class BlockRegistry {
     }
 
     public static void registerFullBlock(String identifier, Block blockInstance, BlockItem itemInstance) {
-        Registry.register(Registry.BLOCK, new Identifier(EldritchEnd.MODID, identifier), blockInstance);
+        Registry.register(Registries.BLOCK, new Identifier(EldritchEnd.MODID, identifier), blockInstance);
         ItemRegistry.reg(itemInstance, identifier);
     }
 
     public static void registerSign(String sign_name, Block SIGN, Block WALL_SIGN) {
-        Registry.register(Registry.BLOCK, new Identifier(EldritchEnd.MODID, sign_name+"_sign"), SIGN);
-        Registry.register(Registry.BLOCK, new Identifier(EldritchEnd.MODID, sign_name+"_wall_sign"), WALL_SIGN);
-        Registry.register(Registry.ITEM, new Identifier(EldritchEnd.MODID, sign_name+"_sign"), PRIMORDIAL_SIGN_ITEM);
+        Registry.register(Registries.BLOCK, new Identifier(EldritchEnd.MODID, sign_name+"_sign"), SIGN);
+        Registry.register(Registries.BLOCK, new Identifier(EldritchEnd.MODID, sign_name+"_wall_sign"), WALL_SIGN);
+        Registry.register(Registries.ITEM, new Identifier(EldritchEnd.MODID, sign_name+"_sign"), PRIMORDIAL_SIGN_ITEM);
     }
 }
