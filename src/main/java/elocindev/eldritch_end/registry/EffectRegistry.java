72a63cffd793fd5ba3b7853386e4206ceb8385a0
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-09-26 13:06:02 +0100

    fix(effect registry class)

--- src/main/java/elocindev/eldritch_end/registry/EffectRegistry.java
+++ src/main/java/elocindev/eldritch_end/registry/EffectRegistry.java
@@ -4,8 +4,9 @@ import elocindev.eldritch_end.EldritchEnd;
 import elocindev.eldritch_end.effects.Corruption;
 import elocindev.eldritch_end.effects.HasturPresence;
 import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class EffectRegistry {
     public static final StatusEffect CORRUPTION = new Corruption();
@@ -13,7 +14,7 @@ public class EffectRegistry {
     public static final StatusEffect HASTUR_PRESENCE = new HasturPresence();
 
     public static void register() {
-        Registry.register(Registry.STATUS_EFFECT, new Identifier(EldritchEnd.MODID, "corruption"), CORRUPTION);
-        Registry.register(Registry.STATUS_EFFECT, new Identifier(EldritchEnd.MODID, "hastur_presence"), HASTUR_PRESENCE);
+        Registry.register(Registries.STATUS_EFFECT, new Identifier(EldritchEnd.MODID, "corruption"), CORRUPTION);
+        Registry.register(Registries.STATUS_EFFECT, new Identifier(EldritchEnd.MODID, "hastur_presence"), HASTUR_PRESENCE);
     }
 }
