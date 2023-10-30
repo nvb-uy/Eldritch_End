6a1fe52a5e0283b5d3e6033825018d6a3551ba9e
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-10-30 22:00:12 +0000

    add(primordial abyss surface feature)

--- src/main/java/elocindev/eldritch_end/registry/WorldgenRegistry.java
+++ src/main/java/elocindev/eldritch_end/registry/WorldgenRegistry.java
@@ -1,8 +1,11 @@
 package elocindev.eldritch_end.registry;
 
 import elocindev.eldritch_end.EldritchEnd;
+import elocindev.eldritch_end.config.entries.PrimordialAbyssConfig;
 import elocindev.eldritch_end.worldgen.feature.PrimordialTreeFeature;
+import elocindev.eldritch_end.worldgen.feature.SurfaceConfig;
 import elocindev.eldritch_end.worldgen.feature.TreeConfig;
+import elocindev.eldritch_end.worldgen.feature.surface.PrimordialAbyssSurface;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -10,9 +13,18 @@ import net.minecraft.world.gen.feature.Feature;
 
 public class WorldgenRegistry {
     public static final Identifier PRIMORDIAL_TREE_ID = new Identifier(EldritchEnd.MODID, "primordial_tree");
+    public static final Identifier PRIMORDIAL_ABYSS_SURFACE_ID = new Identifier(EldritchEnd.MODID, "primordial_abyss_surface");
+    public static final Identifier HASTURIAN_WASTES_SURFACE_ID = new Identifier(EldritchEnd.MODID, "hasturian_wastes_surface");
+    public static final Identifier HASTURIAN_SPIKES_ID = new Identifier(EldritchEnd.MODID, "hasturian_spikes");
+
     public static Feature<TreeConfig> PRIMORDIAL_TREE_FEATURE = new PrimordialTreeFeature(TreeConfig.CODEC);
+    public static Feature<SurfaceConfig> PRIMORDIAL_ABYSS_SURFACE_FEATURE = new PrimordialAbyssSurface(SurfaceConfig.CODEC);
+
 
     public static void register() {
         Registry.register(Registries.FEATURE, PRIMORDIAL_TREE_ID, PRIMORDIAL_TREE_FEATURE);
+        Registry.register(Registries.FEATURE, PRIMORDIAL_ABYSS_SURFACE_ID, PRIMORDIAL_ABYSS_SURFACE_FEATURE);
+//        Registry.register(Registries.FEATURE, HASTURIAN_WASTES_SURFACE_ID, PRIMORDIAL_TREE_FEATURE);
+//        Registry.register(Registries.FEATURE, HASTURIAN_SPIKES_ID, PRIMORDIAL_TREE_FEATURE);
     }
 }
