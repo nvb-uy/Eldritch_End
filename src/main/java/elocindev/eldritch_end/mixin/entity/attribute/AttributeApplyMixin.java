b10ba9217fc97600e41aaca121d4d6a8a8cd0174
nvb-uy <almhak@proton.me>
2023-11-17 12:44:15 -0300

    add(attribute registry to livingentity)

---
+++ src/main/java/elocindev/eldritch_end/mixin/entity/attribute/AttributeApplyMixin.java
@@ -0,0 +1,18 @@
+package elocindev.eldritch_end.mixin.entity.attribute;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import elocindev.eldritch_end.registry.AttributeRegistry;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+
+@Mixin(LivingEntity.class)
+public class AttributeApplyMixin {
+    @Inject(method = "createLivingAttributes", at = @At("RETURN")) 
+    private static void eldritch_end$registerAttributes(CallbackInfoReturnable<DefaultAttributeContainer.Builder> cir) {
+        ((DefaultAttributeContainer.Builder)cir.getReturnValue()).add(AttributeRegistry.CORRUPTION).add(AttributeRegistry.CORRUPTION_RESISTANCE);
+  }
+}
