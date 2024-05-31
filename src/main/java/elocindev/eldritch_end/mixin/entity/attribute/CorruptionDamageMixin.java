34d060e74f6632472f4fe1717f591dc5bea3feff
JRepo42 <JRepo42@users.noreply.github.com>
2024-05-31 14:54:10 +0100

    add(50 corruption 30% dealt damage reduction added)

--- src/main/java/elocindev/eldritch_end/mixin/entity/attribute/CorruptionDamageMixin.java
+++ src/main/java/elocindev/eldritch_end/mixin/entity/attribute/CorruptionDamageMixin.java
@@ -5,7 +5,10 @@ import elocindev.eldritch_end.registry.AttributeRegistry;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.AttributeContainer;
 import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.registry.entry.RegistryEntry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,8 +21,16 @@ public abstract class CorruptionDamageMixin {
     public abstract double getAttributeValue(EntityAttribute attribute);
 
     @Inject(method = "modifyAppliedDamage", at = @At(value = "TAIL"), cancellable = true)
-    protected void modifyAppliedDamage(DamageSource source, float amount, CallbackInfoReturnable<Float> cir) {
+    protected void eldritch_end$damageReceived(DamageSource source, float amount, CallbackInfoReturnable<Float> cir) {
         if (getAttributeValue(AttributeRegistry.CORRUPTION) < 10 || (source.isOf(Corruption.DAMAGE))) return;
         cir.setReturnValue(cir.getReturnValue() * 1.1f);
+    }
+
+    @Inject(method = "modifyAppliedDamage", at = @At(value = "TAIL"), cancellable = true)
+    protected void eldritch_end$inflictedDamage(DamageSource source, float amount, CallbackInfoReturnable<Float> cir) {
+        if (!(source.getAttacker() instanceof PlayerEntity player) ||
+                (player.getAttributeValue(AttributeRegistry.CORRUPTION) < 50) ||
+                source.isOf(Corruption.DAMAGE)) return;
+        cir.setReturnValue(cir.getReturnValue() * 0.7f);
     }
 }
