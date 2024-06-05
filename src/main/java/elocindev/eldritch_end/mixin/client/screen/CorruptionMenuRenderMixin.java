44a0d7b914f1ac3fa3e597e88e84962d41e2d636
nvb-uy <elocindev@lunapixel.gg>
2024-06-05 06:43:28 -0300

    feat(blinking animation)

--- src/main/java/elocindev/eldritch_end/mixin/client/screen/CorruptionMenuRenderMixin.java
+++ src/main/java/elocindev/eldritch_end/mixin/client/screen/CorruptionMenuRenderMixin.java
@@ -23,18 +23,35 @@ public abstract class CorruptionMenuRenderMixin extends AbstractInventoryScreen<
         super(screenHandler, playerInventory, text);
     }
 
+    public ItemStack dummy = new ItemStack(ItemRegistry.CORRUPTION_MENU);
+
     @Inject(method = "drawBackground", at = @At("TAIL"), cancellable = true)
-    protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY, CallbackInfo info) {       
+    protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY, CallbackInfo info) {  
+        String texturePath = "textures/icon/corruption_menu/";
+        long currentTime = System.currentTimeMillis();
+        long animationCycle = 200;
+        long delay = 3000;
+        long timeInCycle = currentTime % (animationCycle + delay);
+
+        int animIndex;
+
+        if (timeInCycle < animationCycle / 2)
+            animIndex = (int) (timeInCycle / (animationCycle / 8)) + 1;
+        else if (timeInCycle < animationCycle)
+            animIndex = 4 - (int) ((timeInCycle - animationCycle / 2) / (animationCycle / 8));
+        else
+            animIndex = 1;
+
         if (client.player == null || 
         CorruptionAPI.CONFIG.show_icon_only_when_corrupted && CorruptionAPI.getCorruptionLevel(client.player) == 0 ) 
             return;
         
-        String currentTexture = "textures/icon/corruption_menu/1.png";
+        texturePath = texturePath+animIndex+".png";
 
-        context.drawTexture(new Identifier(EldritchEnd.MODID, currentTexture), x + 66, y + 69, 0, 0, 11, 11, 11, 11);
+        context.drawTexture(new Identifier(EldritchEnd.MODID, texturePath), x + 66, y + 69, 0, 0, 11, 11, 11, 11);
         
         if (mouseX >= x + 66 && mouseX <= x + 77 && mouseY >= y + 69 && mouseY <= y + 80) {
-            context.drawItemTooltip(textRenderer, new ItemStack(ItemRegistry.CORRUPTION_MENU), mouseX, mouseY);
+            context.drawItemTooltip(textRenderer, dummy, mouseX, mouseY);
         }
     }
 }
