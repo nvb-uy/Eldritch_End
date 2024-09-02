3aa57a8a6d7954673015db2c09621e8f5ea216f0
nvb-uy <elocindev@lunapixel.gg>
2024-09-02 13:12:56 -0300

    fix(ominous eye not spawning)

--- src/main/java/elocindev/eldritch_end/events/PlayerTickEventHandler.java
+++ src/main/java/elocindev/eldritch_end/events/PlayerTickEventHandler.java
@@ -1,6 +1,7 @@
 package elocindev.eldritch_end.events;
 
 import elocindev.eldritch_end.api.CorruptionAPI;
+import elocindev.eldritch_end.corruption.corruption_effect.CEEyeSpawn;
 import elocindev.eldritch_end.corruption.corruption_effect.CETentacleSpawn;
 import elocindev.eldritch_end.utils.ServerUtils;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
@@ -8,13 +9,17 @@ import net.minecraft.server.MinecraftServer;
 
 public class PlayerTickEventHandler implements ServerTickEvents.StartTick {
     private static CETentacleSpawn tentacle_config = CorruptionAPI.CONFIG.corruption_effects.tentacle_spawn;
+    private static CEEyeSpawn eye_config = CorruptionAPI.CONFIG.corruption_effects.ominous_eye_spawn;
 
     @Override
     public void onStartTick(MinecraftServer server) {
-        if (server.getTicks() % 20 != 0) return;
-        ServerUtils.healthDrainCheck(server);
+        if (server.getTicks() % 20 == 0)
+            ServerUtils.healthDrainCheck(server);
 
-        if (server.getTicks() % tentacle_config.getEffectRateTicks() != 0) return;
-        ServerUtils.tentacleSummonCheck(server);
+        if (server.getTicks() % tentacle_config.getEffectRateTicks() == 0)
+            ServerUtils.tentacleSummonCheck(server);
+
+        if (server.getTicks() % eye_config.getEffectRateTicks() == 0)
+            ServerUtils.ominousEyeSummonCheck(server);
     }
 }
--- src/main/java/elocindev/eldritch_end/utils/ServerUtils.java
+++ src/main/java/elocindev/eldritch_end/utils/ServerUtils.java
@@ -2,7 +2,9 @@ package elocindev.eldritch_end.utils;
 
 import elocindev.eldritch_end.api.CorruptionAPI;
 import elocindev.eldritch_end.config.Configs;
+import elocindev.eldritch_end.corruption.corruption_effect.CEEyeSpawn;
 import elocindev.eldritch_end.corruption.corruption_effect.CETentacleSpawn;
+import elocindev.eldritch_end.entity.ominous_eye.OminousEyeEntity;
 import elocindev.eldritch_end.entity.tentacle.TentacleEntity;
 import elocindev.eldritch_end.registry.EntityRegistry;
 import net.minecraft.server.MinecraftServer;
@@ -11,16 +13,19 @@ import net.minecraft.world.World;
 
 public class ServerUtils {
 
-    // Todo: Config; The maximum time window since last attack (in ticks)
-    public static int EYE_THRESHOLD = 75;
-    public static int HEALTH_DRAIN_THRESHOLD = 100;
-
     private static CETentacleSpawn TENTACLE_CFG = Configs.Mechanics.CORRUPTION.corruption_effects.tentacle_spawn;
+    private static CEEyeSpawn EYE_CFG = Configs.Mechanics.CORRUPTION.corruption_effects.ominous_eye_spawn;
 
     public static boolean hasValidTentacleConditions(ServerPlayerEntity serverPlayer) {        
-        return CorruptionAPI.getTotalCorruptionLevel(serverPlayer) >= TENTACLE_CFG.getStartingLevel()
+        return CorruptionAPI.getAffectedCorruptionLevel(serverPlayer) >= TENTACLE_CFG.getStartingLevel()
                 && (serverPlayer.age - serverPlayer.getLastAttackTime() <= TENTACLE_CFG.getCombatDurationTicks())
                 && serverPlayer.getRandom().nextFloat() <= TENTACLE_CFG.getSpawnChance();
+    }
+
+    public static boolean hasValidEyeConditions(ServerPlayerEntity serverPlayer) {
+        return CorruptionAPI.getAffectedCorruptionLevel(serverPlayer) >= EYE_CFG.getStartingLevel()
+                && serverPlayer.age - serverPlayer.getLastAttackTime() >= 100
+                && serverPlayer.getRandom().nextFloat() <= EYE_CFG.getSpawnChance();
     }
 
     public static void summonTentacle(ServerPlayerEntity serverPlayer, World world) {
@@ -35,9 +40,21 @@ public class ServerUtils {
         serverPlayer.velocityModified = true;
     }
 
+    public static void summonOminousEye(ServerPlayerEntity serverPlayer, World world) {
+        for (int i = 0; i <= EYE_CFG.getEyeAmount(); i++) {
+            OminousEyeEntity ominousEye = EntityRegistry.OMINOUS_EYE.create(world);
+
+            if (EYE_CFG.getStartingLevel() == -1 || ominousEye == null || serverPlayer == null || !serverPlayer.isOnGround()) return;
+
+            ominousEye.setPosition(serverPlayer.getPos().add(0, 1, 0));
+            world.spawnEntity(ominousEye);
+            ominousEye.setTarget(serverPlayer);
+        }
+    }
+
     public static void healthDrainCheck(MinecraftServer server) {
         for (ServerPlayerEntity serverPlayer: server.getPlayerManager().getPlayerList()) {
-            if (CorruptionAPI.getTotalCorruptionLevel(serverPlayer) >= HEALTH_DRAIN_THRESHOLD) {
+            if (CorruptionAPI.getAffectedCorruptionLevel(serverPlayer) >= Configs.Mechanics.CORRUPTION.corruption_effects.madness_consumed.getStartingLevel()) {
                 serverPlayer.damage(serverPlayer.getDamageSources().generic(), serverPlayer.getMaxHealth() * 0.1f);
             }
         }
@@ -47,6 +64,14 @@ public class ServerUtils {
         for (ServerPlayerEntity serverPlayer: server.getPlayerManager().getPlayerList()) {
             if (ServerUtils.hasValidTentacleConditions(serverPlayer)) {
                 ServerUtils.summonTentacle(serverPlayer, serverPlayer.getWorld());
+            }
+        }
+    }
+
+    public static void ominousEyeSummonCheck(MinecraftServer server) {
+        for (ServerPlayerEntity serverPlayer: server.getPlayerManager().getPlayerList()) {
+            if (ServerUtils.hasValidEyeConditions(serverPlayer)) {
+                ServerUtils.summonOminousEye(serverPlayer, serverPlayer.getWorld());
             }
         }
     }
