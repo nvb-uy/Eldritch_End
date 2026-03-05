946b295039a5c05a83ac98f8be88d69ec1e1795c
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-03-04 20:10:14 -0600

    Fixed Aberrations and Dendlers spawning right above the void

--- src/main/java/elocindev/eldritch_end/mixin/worldgen/EntFixerMixin.java
+++ src/main/java/elocindev/eldritch_end/mixin/worldgen/EntFixerMixin.java
@@ -6,21 +6,22 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import elocindev.eldritch_end.entity.aberration.AberrationEntity;
-import net.minecraft.block.Blocks;
+import elocindev.eldritch_end.entity.dendler.DendlerEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3i;
 
 @Mixin(ServerWorld.class)
 public class EntFixerMixin {
     @Inject(method = "addEntity", at = @At("HEAD"), cancellable = true)
     private void eldritch_end$fixSpawn(Entity entity, CallbackInfoReturnable<Boolean> info) {
-        if (!(entity instanceof AberrationEntity ent)) return;
+        if (!(entity instanceof AberrationEntity) && !(entity instanceof DendlerEntity)) return;
 
-        if (((ServerWorld)(Object)this).getBlockState(
-            new BlockPos(new Vec3i((int)ent.getPos().x, (int)ent.getPos().y, (int)ent.getPos().z)).down(5)
-            ).isOf(Blocks.AIR)
-        ) info.setReturnValue(false);
+        ServerWorld world = (ServerWorld) (Object) this;
+        BlockPos pos = entity.getBlockPos().down();
+
+        if (!world.getBlockState(pos).allowsSpawning(world, pos, entity.getType())) {
+            info.setReturnValue(false);
+        }
     }
 }
